d2fbc2b84b36eebc2681335042bcd140af4bb608
iqramqra <masrooriqra@gmail.com>
2020-05-30 22:11:31 -0400

    connected to frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password)
+        params.permit(:username, :password, :avatar)
     end
 
 end
